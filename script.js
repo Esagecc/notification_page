e00eac8c447b5f85be0e85745a3eb3efab8aa9a9
Elizabeth Cantu <e.sage.cc@gmail.com>
2022-10-12 22:05:13 -0500

    added the active state

---
+++ script.js
@@ -0,0 +1,11 @@
+let mark = document.querySelector('.mark')
+let number = document.querySelector('.number')
+let dots = document.querySelectorAll('.red-dot')
+let unread = document.querySelectorAll('.unread')
+
+
+mark.addEventListener('click', read)
+
+function read(){
+    number.querySelector
+}
